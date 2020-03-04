748d411aba1795d35bbb81132f07b9cbd997273f
Josh Ellis <joshe523@gmail.com>
2020-03-04 23:28:45 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,4 +15,8 @@ class Doctor
   def appointments
     Appointment.all.select{|appt| appt.doctor == self}
   end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
 end
