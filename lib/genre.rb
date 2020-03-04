e8279866961a9f88f5cd26404afdd79bfac618a1
Josh Ellis <joshe523@gmail.com>
2020-03-04 23:20:10 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -6,3 +6,12 @@ class Genre
   def self.all
     @@all
   end
+
+  def initialize(name)
+    @name = name
+    Genre.all << self
+  end
+
+  def artists
+    Artist.all.select{|song| song.genre == self}
+  end
